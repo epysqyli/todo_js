df9e58f8d101601ef407230112f7933b5332797e
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 17:38:11 +0200

    Remove creation and dom js, to be added if and when needed

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
 
-import createItem from './dom';
+import builder from "./builder";
 
+let addProject = document.getElementById('add-project');
+addProject.addEventListener("click", () => {
+  let projectName = prompt("Enter a project name");
+  builder.createProject(projectName);
+  content.innerHTML += builder.projects[projectName].name;
+})
