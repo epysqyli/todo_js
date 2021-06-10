d7a11bab89dbd722ac7987e24f700151db3c04f5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 12:30:14 +0200

    Add example object creation (to be removed)

--- src/builders.js
+++ src/builders.js
@@ -33,4 +33,4 @@ const addItemToProject = (item, project) => {
 };
 
 // export all functions and objects
-export default {createToDo, createProject, addItemToProject, projects};
+export default { createToDo, createProject, addItemToProject, projects };
