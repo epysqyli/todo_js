abdc8314bad02e978048dc80444bc65147bff75e
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 21:57:39 +0200

    Add navbar within items-container

--- src/index.js
+++ src/index.js
@@ -4,9 +4,13 @@ import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
 
 import builder from "./builder";
+import removeAllChildren from "./helpers";
 
 //function for adding projects
 const addProject = (projectName) => {
+  if (projectName == '') {
+    return;
+  }
   builder.createProject(projectName);
   let projectsList = document.getElementById("projects-list");
   let newProject = document.createElement("div");
@@ -15,9 +19,18 @@ const addProject = (projectName) => {
   projectsList.appendChild(newProject);
 };
 
+// define click event listener on add button for new projects
+const addBtn = document.getElementById("add-btn");
+addBtn.addEventListener("click", () => {
+  let projectName = document.getElementById("project-name");
+  addProject(projectName.value);
+  projectName.value = '';
+});
+
+// add default project
 addProject("Default");
 
-const addBtn = document.getElementById('add-btn');
-addBtn.addEventListener("click", () => {
-  addProject(document.getElementById('project-name').value);
-})
+let testItem = builder.createToDo("test Item", "Some stuff and some details", "sometime");
+builder.addItemToProject(testItem, builder.projects["Default"]);
+
+// populate items-container with project title, project todoes as boxes and a way to create new todoes
