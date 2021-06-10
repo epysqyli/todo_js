15d4234cad311500dc4d10e597c2b508f3f6655d
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 22:46:19 +0200

    Add event listener on a sidebar projects

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import removeAllChildren from "./helpers";
 
 //function for adding projects
 const addProject = (projectName) => {
-  if (projectName == '') {
+  if (projectName == "") {
     return;
   }
   builder.createProject(projectName);
@@ -22,15 +22,36 @@ const addProject = (projectName) => {
 // define click event listener on add button for new projects
 const addBtn = document.getElementById("add-btn");
 addBtn.addEventListener("click", () => {
+  i++; // to avoid duplicating event listeners
   let projectName = document.getElementById("project-name");
   addProject(projectName.value);
-  projectName.value = '';
+  projectName.value = "";
+  addProjectListener();
 });
 
 // add default project
 addProject("Default");
 
-let testItem = builder.createToDo("test Item", "Some stuff and some details", "sometime");
+let testItem = builder.createToDo(
+  "test Item",
+  "Some stuff and some details",
+  "sometime"
+);
 builder.addItemToProject(testItem, builder.projects["Default"]);
 
 // populate items-container with project title, project todoes as boxes and a way to create new todoes
+const displayProject = (projectName) => {
+  let projectTitle = document.getElementById("title");
+  projectTitle.innerHTML = projectName.name;
+};
+
+let i = 0; // variable used to avoid duplicating event listeners on previous projects after adding a new one
+
+const addProjectListener = () => {
+  let projectsArray = Array.from(document.querySelectorAll(".project"));
+  projectsArray.slice(i, projectsArray.length).forEach((project) =>
+    project.addEventListener("click", (e) => console.log(e.target))
+  );
+};
+
+addProjectListener();
