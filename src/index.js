e26e50f06738a2b3cd50ab1716dba2b96fda5c59
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 18:40:53 +0200

    Add addProject function and addBtn event listener

--- src/index.js
+++ src/index.js
@@ -5,10 +5,20 @@ import "@fortawesome/fontawesome-free/js/brands";
 
 import builder from "./builder";
 
-// create default Project and add it to the DOM --> make a function for this
-builder.createProject("defaultProject");
-let projectsList = document.getElementById("projects");
-let newProject = document.createElement("div");
-newProject.classList.add('project');
-newProject.innerHTML = builder.projects["defaultProject"].name;
-projectsList.appendChild(newProject);
+//function for adding projects
+const addProject = (projectName) => {
+  builder.createProject(projectName);
+  let projectsList = document.getElementById("projects");
+  let newProject = document.createElement("div");
+  newProject.classList.add("project");
+  newProject.innerHTML = builder.projects[projectName].name;
+  projectsList.appendChild(newProject);
+};
+
+addProject("Default");
+
+const addBtn = document.getElementById('add-btn');
+addBtn.addEventListener("click", () => {
+  let projectName = document.getElementById('project-name').value;
+  addProject(projectName);
+})
