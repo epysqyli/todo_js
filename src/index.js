b3cc6c5eeae4674e7590e71433576425f17925ad
elvis.pysqyli <elvis.pysqyli@gmail.com>
2021-06-10 10:45:57 +0200

    Rename todoes to builders

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
-import builders from './todoes';
-
+import "./builders";
+import builders from "./builders";
